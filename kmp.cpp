27f19031b94a529beb6500a3cdbdd9d8fb6b402f
Jeffrey0418 <blueskyzzz0418@gmail.com>
2022-07-03 22:42:41 +0800

    update kmp.cpp

--- kmp.cpp
+++ kmp.cpp
@@ -1,41 +1,30 @@
-void getNext(char * p, int * next)
-{
-	next[0] = -1;
-	int i = 0, j = -1;
+// T28. й╣ож strStr()
 
-	while (i < strlen(p) - 1)
-	{
-		if (j == -1 || p[i] == p[j])
-		{
-			++i;
-			++j;
-			next[i] = j;
-		}	
-		else
-			j = next[j];
-	}
-}
-
-int strStr(char * haystack, char * needle){
-    if (strlen(needle) == 0) return 0;
-    int * next = (int*)malloc(sizeof(int) * strlen(needle));
-    getNext(needle, next);
-    int i = 0;
-	int j = 0;
-
-	while (i < strlen(haystack) && j < (int)strlen(needle))
-	{
-		if (j == -1 || haystack[i] == needle[j]) 
-		{
-			i++;
-           	j++;
-		}
-	 	else 
-           	j = next[j];
-    }
-
-    if (j == strlen(needle))
-        return i - j;
-    else 
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int n = haystack.size(), m = needle.size();
+        if (m == 0)
+            return 0;
+        vector<int> pi(m);
+        for (int i = 1, j = 0; i < m; i++)
+        {
+            while (j > 0 && needle[i] != needle[j])
+                j = pi[j - 1];
+            if (needle[i] == needle[j])
+                j++;
+            pi[i] = j;
+        }
+        for (int i = 0, j = 0; i < n; i++)
+        {
+            while (j > 0 && haystack[i] != needle[j])
+                j = pi[j - 1];
+            if (haystack[i] == needle[j])
+                j++;
+            if (j == m)
+                return i - m + 1;
+        }
         return -1;
-}
+    }
+};
+
